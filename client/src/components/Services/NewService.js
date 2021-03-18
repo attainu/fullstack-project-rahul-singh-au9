e70f92b5b8ec7fc42dfccfb32d94538e82e649ea
unknown <rahulsg1508@gmail.com>
2021-03-18 16:21:22 +0530

    successfully posting services

--- client/src/components/Services/NewService.js
+++ client/src/components/Services/NewService.js
@@ -1,24 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { TextField, Button, Typography, Paper } from '@material-ui/core';
-// import { DatePicker } from '@material-ui/pickers'
-import { useDispatch, useSelector } from 'react-redux';
-import FileBase from 'react-file-base64';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import {toast} from 'react-toastify';
-import AlgoliaPlaces from 'algolia-places-react';
-// import moment from 'moment';
-import useStyles from './styles';
 import {createService} from '../../actions/serviceActions';
+import NewServiceForm from './NewServiceForm';
 
-const config = {
-    appId: process.env.REACT_APP_ALGOLIA_APP_ID,
-    apiKey: process.env.REACT_APP_ALGOLIA_API_KEY,
-    language: 'en',
-    countries: ['in']
-}
 
 const NewService = () => {
 
-    const classes = useStyles();
     const {auth} = useSelector((state) => ({...state}));
     const [values, setValues] = useState({
         title: '',
@@ -33,145 +21,35 @@ const NewService = () => {
 
     const [location, setLocation] = useState("");
 
-    // destructing values from state
-    const {title, content, image, price, from, to, option, total} = values;
 
     const clear = () => {
-        setValues({ title: "", content: "", location: "", image: "", price: "", from: "", to: "", options: ""  });
+        setValues({ title: "", content: "", image: "", price: "", from: "", to: "", options: ""  });
+        setLocation("")
     };
 
     const handleSubmit = async (e) => {
-        e.preventDefault()
-        let res = await createService(auth.token, {...values, location, postedBy: auth.result.name});
-        // console.log("NEW SERVICE ====>", res)
-        toast.success("Your New Service is Posted Successfully...")
-        clear();
+        try{
+            e.preventDefault()
+            let res = await createService(auth.token, {...values, location, postedBy: auth.result.name});
+            // console.log("NEW SERVICE ====>", res)
+            toast.success("Your New Service is Posted Successfully...")
+            clear();
+        } catch(err){
+            toast.error(err.response.data)
+        }
+
     };
 
-    const minDate = new Date(Date.now());
-
     return (
+        <NewServiceForm
+        values={values}
+        setValues={setValues}
+        location={location}
+        setLocation={setLocation}
+        handleSubmit={handleSubmit}
+        clear={clear}
+        />
 
-        <Paper className={classes.paper}>
-
-            <form
-            autoComplete="off"
-            noValidate
-            className={`${classes.root} ${classes.form}`}
-            onSubmit={handleSubmit}
-            >
-                <Typography variant="h6">
-                    Add A New Service
-                </Typography>
-
-                <TextField
-                name="title"
-                variant="outlined"
-                label="Title"
-                fullWidth
-                value={title}
-                onChange={(e) => setValues({ ...values, title: e.target.value })}
-                />
-
-                <TextField
-                name="content"
-                variant="outlined"
-                label="Content"
-                fullWidth multiline
-                rows={4}
-                value={content}
-                onChange={(e) => setValues({ ...values, content: e.target.value })}
-                />
-
-                <AlgoliaPlaces
-
-                name='location'
-                value={location}
-                placeholder='Location'
-                options={config}
-                // onChange = {({suggestion}) => setValues({...values, location: suggestion.value})}
-                onChange={({ suggestion }) => setLocation(suggestion.value)}
-                style={{ height:"60px" ,width:"1183px", marginTop:"8px" }}
-                />
-
-                <TextField
-                name="price"
-                variant="outlined"
-                type="Number"
-                label="Price"
-                fullWidth
-                value={price}
-                onChange={(e) => setValues({ ...values, price: e.target.value })}
-                />
-
-                <TextField
-                name="total"
-                variant="outlined"
-                type="Number"
-                label="Number of bookings"
-                fullWidth
-                value={total}
-                onChange={(e) => setValues({ ...values, total: e.target.value })}
-                />
-
-                <TextField
-                label="From"
-                name="from"
-                type="date"
-                defaultValue={minDate}
-                value={from}
-                onChange={(e) => setValues({ ...values, from: e.target.value })}
-                className={classes.textField}
-                InputLabelProps={{
-                shrink: true,
-                }}
-                />
-
-                <TextField
-                label="To"
-                name="to"
-                type="date"
-                value={to}
-                onChange={(e) => setValues({ ...values, to: e.target.value })}
-                defaultValue={minDate}
-                className={classes.textField}
-                InputLabelProps={{
-                shrink: true,
-                }}
-                />
-
-                <div className={classes.fileInput}>
-                    <FileBase
-                    type="file"
-                    multiple={false}
-                    onDone={({ base64 }) => setValues({ ...values, image: base64 })}
-                    />
-                </div>
-
-                <Button
-                className={classes.buttonSubmit}
-                variant="contained"
-                color="primary"
-                size="large"
-                type="submit"
-                fullWidth
-                >
-                Submit
-                </Button>
-
-                <Button
-                variant="contained"
-                color="secondary"
-                size="small"
-                onClick={clear}
-                fullWidth
-                >
-                Clear
-                </Button>
-            </form>
-            {/* <pre>{JSON.stringify(values, null, 4)}</pre>
-                {JSON.stringify(location)} */}
-        </Paper>
     );
 };
 
